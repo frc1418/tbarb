8c6a26453c093549dde58ce616985df9f7224b65
ErikBoesen <me@erikboesen.com>
2017-12-16 17:49:42 -0500

    Rename 'fetch' => 'get', send key as header rather than URL parameter

--- lib/main.rb
+++ lib/main.rb
@@ -5,124 +5,132 @@ require 'json'
 class TBA
 
     @@API_ROOT = 'https://www.thebluealliance.com/api/v2/'
-    @@URL_POST = '?X-TBA-App-Id='
     @api_key = ''
 
-    def initialize(id)
-        @app_id = id
+    def initialize(key)
+        @api_key = key
     end
 
-    def fetch(url)
-        JSON.parse(Net::HTTP.get(URI.parse(@@API_ROOT + url + @@URL_POST + @app_id)))
+    def get(path)
+        uri = URI(@@API_ROOT + path)
+
+        Net::HTTP.start(uri.host, uri.port,
+            :use_ssl => uri.scheme == 'https') do |http|
+            request = Net::HTTP::Get.new(uri)
+            request.add_field("X-TBA-App-Id", @api_key)
+
+            response = http.request request # Net::HTTPResponse object
+            return JSON.parse(response.body)
+        end
     end
 
     def get_teams(index)
-        fetch("teams/#{index}")
+        get("teams/#{index}")
     end
 
     def get_team(team)
-        fetch("team/frc#{team}")
+        get("team/frc#{team}")
     end
 
     def get_team_events(team, year)
-        fetch("team/frc#{team}/#{year}/events")
+        get("team/frc#{team}/#{year}/events")
     end
 
     def get_team_awards(team, *event)
         if event[0]
-            fetch("team/frc#{team}/event/#{event[0]}/awards")
+            get("team/frc#{team}/event/#{event[0]}/awards")
         else
-            fetch("team/frc#{team}/awards")
+            get("team/frc#{team}/awards")
         end
     end
 
     def get_team_matches(team, event)
-        fetch("team/frc#{team}/event/#{event}/matches")
+        get("team/frc#{team}/event/#{event}/matches")
     end
 
     def get_team_years(team)
-        fetch("team/frc#{team}/years_participated")
+        get("team/frc#{team}/years_participated")
     end
 
     def get_team_media(team, *year)
         if year[0]
-            fetch("team/frc#{team}/#{year[0]}/media")
+            get("team/frc#{team}/#{year[0]}/media")
         else
-            fetch("team/frc#{team}/media")
+            get("team/frc#{team}/media")
         end
     end
 
     def get_team_history_events(team)
-        fetch("team/frc#{team}/history/events")
+        get("team/frc#{team}/history/events")
     end
 
     def get_team_history_awards(team)
-        fetch("team/frc#{team}/history/awards")
+        get("team/frc#{team}/history/awards")
     end
 
     def get_team_history_robots(team)
-        fetch("team/frc#{team}/history/robots")
+        get("team/frc#{team}/history/robots")
     end
 
     def get_team_history_districts(team)
-        fetch("team/frc#{team}/history/districts")
+        get("team/frc#{team}/history/districts")
     end
 
     def get_event_list(*year)
         if year[0]
-            fetch("events/#{year}")
+            get("events/#{year}")
         else
-            fetch("events/")
+            get("events/")
         end
     end
 
     def get_event(event)
-        fetch("event/#{event}")
+        get("event/#{event}")
     end
 
     def get_event_teams(event)
-        fetch("event/#{event}/teams")
+        get("event/#{event}/teams")
     end
 
     def get_event_stats(event)
-        fetch("event/#{event}/stats")
+        get("event/#{event}/stats")
     end
 
     def get_event_rankings(event)
-        fetch("event/#{event}/rankings")
+        get("event/#{event}/rankings")
     end
 
     def get_event_awards(event)
-        fetch("event/#{event}/awards")
+        get("event/#{event}/awards")
     end
 
     def get_event_matches(event)
-        fetch("event/#{event}/matches")
+        get("event/#{event}/matches")
     end
 
     def get_district_points(event)
-        fetch("event/#{event}/district_points")
+        get("event/#{event}/district_points")
     end
 
     # TODO: Make this a bit more accessible. Add automatic key generation, etc.
     def get_match(match)
-        fetch("match/#{match}")
+        get("match/#{match}")
     end
 
     def get_districts(year)
-        fetch("districts/#{year}")
+        get("districts/#{year}")
     end
 
     def get_district_events(district, year)
-        fetch("district/#{district}/#{year}/events")
+        get("district/#{district}/#{year}/events")
     end
 
     def get_district_rankings(district, year)
-        fetch("district/#{district}/#{year}/rankings")
+        get("district/#{district}/#{year}/rankings")
     end
 
     def get_district_teams(district, year)
-        fetch("district/#{district}/#{year}/teams")
+        get("district/#{district}/#{year}/teams")
     end
 
 end
